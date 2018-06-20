4d69a1fc2697967cb3288936c9540831658a880c
songhailong <997496837@qq.com>
2018-06-20 11:00:47 +0800

    配合cocopod

---
+++ HLPlayer/HLPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HLPlayerDemo
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/6/20.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HLPlayer/HLPlayerDemoTests/HLPlayerDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  HLPlayerDemoTests.m
+//  HLPlayerDemoTests
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/6/20.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface HLPlayerDemoTests : XCTestCase
+
+@end
+
+@implementation HLPlayerDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
