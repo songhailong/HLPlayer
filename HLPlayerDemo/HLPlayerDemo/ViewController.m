8b636ec3b00c150ba49523572ed6545022521b71
songhailong <997496837@qq.com>
2018-06-20 10:20:36 +0800

    初始化项目

---
+++ HLPlayerDemo/HLPlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HLPlayerDemo
+//
+//  Created by 靓萌服饰靓萌服饰 on 2018/6/20.
+//  Copyright © 2018年 靓萌服饰靓萌服饰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
